1f404c1ae87b974c2087e22177f7b3f287220721
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2026-03-04 16:46:38 -0500

    Add custom `tokio_test` test utility

---
+++ src/test_utils.rs
@@ -0,0 +1,13 @@
+use anyhow::{Context, Result};
+
+/// Replaces `#[tokio::test]`, not inserting `#[allow(clippy::expect_used)]`.
+///
+/// Based on the "equivalent code" listed in the docs at
+/// <https://docs.rs/tokio/latest/tokio/attr.test.html#using-current-thread-runtime>
+pub fn tokio_test<F: Future<Output = Result<()>>>(f: F) -> Result<()> {
+    tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .context("failed to set up Tokio runtime for test")?
+        .block_on(f)
+}
